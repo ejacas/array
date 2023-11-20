5fd45379cffb3de9211700e917d2220169f23d1f
ejacas <evaldasjacas.ej@gmail.com>
2023-11-20 15:33:28 +0200

    init

---
+++ script.js
@@ -0,0 +1,31 @@
+// Array 1 Užduotis
+
+var characters = ["Jon Snow", "Cersei Lannister", "Daenerys Targaryen", "Theon Greyjoy", "Tyrion Lannister", "Arya Stark"]
+
+console.log(characters[0])
+console.log(characters[characters.length -1])
+
+var num = prompt("Įveskite skaičių nuo 1 iki 6")
+
+switch(num){
+    case "1" :
+    console.log(characters[0])
+    break
+    case "2" :
+    console.log(characters[1])
+    break
+    case "3" :
+    console.log(characters[2])
+    break
+    case "4" :
+    console.log(characters[3])
+    break
+    case "5" :
+    console.log(characters[4])
+    break
+    case "6" :
+    console.log(characters[5])
+    break
+}
+
+// 2 Užduotis
